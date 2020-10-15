a76dfbc859f517dfa2112910f78c6c255f2e3f76
Layna Hong <laynahong@Laynas-MacBook-Air.local>
2020-10-15 16:38:38 -0400

    finished javascript

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 /* Navbar */
 
 // get the hamburger element
+var hamburger = document.getElementById("hamburger");
 
 // get the menu element
+var menu = document.getElementsById("menu");
 
 // get the overlay element
+var overlay = document.getElementsById("overlay");
 
 // define toggle function
+function toggleMenu() {
+  // either adds or removes active class from element //
+  menu.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
 
 // execute toggle function from hamburger on click
+hamburger.addEventListener("click", toggleMenu);
+
+// execute toggle function from menu on click //
+menu.addEventListener("click", toggleMenu);
 
 // exuecute toggle function from overlay on click
+overlay.addEventListener("click", toggleMenu);
 
 /* Navbar */
